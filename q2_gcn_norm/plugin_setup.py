a7408c18e33911ebca3519cd95c8e4c54478bb4e
Jiung-Wen <30737103+Jiung-Wen@users.noreply.github.com>
2019-12-01 02:44:08 +0800

    Update plugin_setup.py

--- q2_gcn_norm/plugin_setup.py
+++ q2_gcn_norm/plugin_setup.py
@@ -8,11 +8,11 @@ plugin = Plugin(
     version='2019.11',
     website='https://github.com/Jiung-Wen/q2-gcn-norm',
     package='q2_gcn_norm',
-    description=('This QIIME 2 plugin normalizes sequences by 16S rRNA gene copy number (GCN)'
-                 'based on rrnDB database (version 5.6). The plugin matches the taxa of sequences'
-                 'with the rrnDB-5.6_pantaxa_stats_NCBI.tsv file, starting from the lowest taxonomic rank.'
-                 'If a match is found, the mean of GCN for the taxon is assigned; if not, the plugin'
-                 'will try to match a higher rank until the highest rank is met. All the unassigned'
+    description=('This QIIME 2 plugin normalizes sequences by 16S rRNA gene copy number (GCN) '
+                 'based on rrnDB database (version 5.6). The plugin matches the taxa of sequences '
+                 'with the rrnDB-5.6_pantaxa_stats_NCBI.tsv file, starting from the lowest taxonomic rank. '
+                 'If a match is found, the mean of GCN for the taxon is assigned; if not, the plugin '
+                 'will try to match a higher rank until the highest rank is met. All the unassigned '
                  'sequences are assumed to have one GCN.'),
     short_description='This plugin normalizes sequences by 16S rRNA gene copy number (GCN).',
 )
